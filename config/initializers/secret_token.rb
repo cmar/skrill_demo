aa77227585ea7aa15c7e143c06ace3faa2048dec
cmar <chris@spreecommerce.com>
2012-01-30 08:49:30 -0500

    fresh spree store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkrillDemo::Application.config.secret_token = 'cdc08ee396c12bb7db46a9db9f8377e269eda65cafbcac7b93da035545b64420482ca22464ec58a444f0c99a6f1e6f018963a8491a815be0a29f11f53200e34f'
